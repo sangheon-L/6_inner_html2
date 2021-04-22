7f26feac11c6112ba6268abc016ac84b6abe6966
snagheon-L <tkdgjs0107@naver.com>
2021-04-22 17:08:09 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 6_inner_html2
+ 4.5_javascript
---
+++ js/common.js
@@ -0,0 +1,41 @@
+var name, ageTotal, heightTotal, scoreTotal, ageAvg, heightAvg, scoreAvg;
+var countLeng = 5;
+
+
+/*이름변수선언*/
+var name_1, name_2, name_3, name_4, name_5;
+name_1 = 홍길동; name_2 = 박길동; name_3 = 김길동; name_4 = 이길동; name_5 = 최길동;
+
+
+/*나이변수선언*/
+var age_1, age_2, age_3, age_4, age_5;
+age_1 = 30; age_2 = 18; age_3 = 21; age_4 = 35; age_5 = 38;
+
+/*키변수선언*/
+var height_1, height_2, height_3, height_4, height_5;
+height_1 = 175; height_2 = 180; height_3 = 190; height_4 = 185; height_5 = 178;
+
+/*점수변수선언*/
+var score_1, score_2, score_3, score_4, score_5;
+score = 30; score = 18; score = 21; score = 35; score = 38;
+
+/*나이합산*/
+ageTotal = age_1 + age_2 + age_3 + age_4 + age_5;
+/*키합산*/
+heightTotal = height_1 + height_2 + height_3 + height_4 + height_5;
+/*점수합산*/
+scoreTotal = score_1 + score_2 + score_3 + score_4 + score_5;
+
+
+// 나이 평균
+ageAvg = ageTotal/coutLeng;
+// 키평균
+heightAvg = heightTotal/coutLeng;
+// 점수평균
+heightAvg = scoreTotal/coutLeng;
+
+
+
+list += '<table>';
+
+list += '<table>';
